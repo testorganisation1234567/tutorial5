40db218f6f274cd8ff0dff7a1be4a6e91d1876b9
dmnk1308 <dominikbe@aol.com>
2024-06-05 17:49:22 +0200

    Refactoring quiz 2

--- clean_code_quiz2.py
+++ clean_code_quiz2.py
@@ -1,23 +1,21 @@
 # Binary Search in python
 
 
-def binarySearch(array, x, a, b):
+def binarySearch(array, x, low, high):
     # Repeat until the pointers low and high meet each other
-    while a <= b:
-        # middle assigned to lowest element + (highest element - lowest element) / 2
-        c = a + (b - a) // 2
-        if array[c] == x:
-            return c
-        elif array[c] < x:
-            a = c + 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if array[mid] == x:
+            return mid
+        elif array[mid] < x:
+            low = mid + 1
         else:
-            b = c - 1
+            high = mid - 1
     return -1
 
 
 array = [3, 4, 5, 6, 7, 8, 9]
 x = 4
-
 result = binarySearch(array, x, 0, len(array) - 1)
 
 if result != -1:
